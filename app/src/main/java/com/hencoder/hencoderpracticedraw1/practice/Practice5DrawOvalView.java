f98e5013969a6d8d984db9c57da927dedf6aab53
ddancn <703654996@qq.com>
2019-08-06 17:38:17 +0800

    基础图形绘制

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice5DrawOvalView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice5DrawOvalView.java
@@ -2,6 +2,7 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -20,10 +21,13 @@ public class Practice5DrawOvalView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    Paint paint = new Paint();
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawOval() 方法画椭圆
+        canvas.drawOval(100, 100, 300, 200, paint);
     }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice8DrawArcView.java
@@ -2,6 +2,7 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
@@ -20,10 +21,20 @@ public class Practice8DrawArcView extends View {
         super(context, attrs, defStyleAttr);
     }
 
+    Paint paint = new Paint();
+
     @Override
     protected void onDraw(Canvas canvas) {
         super.onDraw(canvas);
 
 //        练习内容：使用 canvas.drawArc() 方法画弧形和扇形
+        paint.setAntiAlias(true);
+        paint.setStyle(Paint.Style.STROKE);
+        canvas.drawArc(100, 100, 400, 300, 180, 60, false, paint);
+
+        paint.setStyle(Paint.Style.FILL);
+        canvas.drawArc(100, 100, 400, 300, 250, 100, true, paint);
+
+        canvas.drawArc(100, 100, 400, 300, 15, 150, false, paint);
     }
 }
